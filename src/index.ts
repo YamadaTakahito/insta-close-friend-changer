15b70683276381469d7dc67b923acac229a984c8
YamadaTakahito <d.p.t.a.d.fighter@gmail.com>
2022-12-30 12:16:55 +0900

    add closeFriend

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+import {IgApiClient} from "instagram-private-api";
+
+const ig = new IgApiClient();
+ig.state.generateDevice(process.env.IG_USERNAME!);
+
+const login = async () => {
+  await ig.simulate.preLoginFlow();
+  const loggedInUser = await ig.account.login(process.env.IG_USERNAME!, process.env.IG_PASSWORD!);
 }
 
-console.log(hello("World"));
+const getCloseFriends = async () => {
+  const feed = ig.feed.bestFriendships()
+  const items = await feed.items();
+  console.log(items)
+  for (const item of items) {
+    console.log("AAAAAAAAAAAA")
+    console.log(item.username)
+    console.log(item.is_bestie)
+    console.log(item.full_name)
+    console.log(item.profile_pic_id)
+  }
+}
+
+const removeAllCloseFriend = async () => {
+  const removeFriends = [1]
+  await ig.friendship.setBesties({
+    remove: removeFriends
+  })
+}
+
+const main = async () => {
+  await login()
+  await getCloseFriends()
+}
+
+main()
