9660abbfba409a04c6d3c27a96fbe03b565c5676
YamadaTakahito <d.p.t.a.d.fighter@gmail.com>
2022-12-30 13:20:38 +0900

    add getFollowings

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,79 @@
 import {IgApiClient} from "instagram-private-api";
+import * as fs from "fs";
+import {stringify} from "csv-stringify/sync";
+import {AccountRepositoryLoginResponseLogged_in_user} from "instagram-private-api/dist/responses";
 
-const ig = new IgApiClient();
-ig.state.generateDevice(process.env.IG_USERNAME!);
-
-const login = async () => {
-  await ig.simulate.preLoginFlow();
-  const loggedInUser = await ig.account.login(process.env.IG_USERNAME!, process.env.IG_PASSWORD!);
+type Friend = {
+  pk: number
+  username: string
+  full_name: string
+  profile_pic_url: string
 }
 
-const getCloseFriends = async () => {
+// @ts-ignore
+let ig: IgApiClient = undefined;
+// @ts-ignore
+let user: AccountRepositoryLoginResponseLogged_in_user = undefined;
+
+const login = async () => {
+  ig = new IgApiClient();
+  ig.state.generateDevice(process.env.IG_USERNAME!);
+  // await ig.simulate.preLoginFlow();
+  user = await ig.account.login(process.env.IG_USERNAME!, process.env.IG_PASSWORD!);
+}
+
+const getCloseFriends = async (): Promise<Friend[]> => {
   const feed = ig.feed.bestFriendships()
   const items = await feed.items();
-  console.log(items)
+  let friends: Friend[] = [];
   for (const item of items) {
-    console.log("AAAAAAAAAAAA")
-    console.log(item.username)
-    console.log(item.is_bestie)
-    console.log(item.full_name)
-    console.log(item.profile_pic_id)
+    friends.push({
+        username: item.username,
+        full_name: item.full_name,
+        pk: item.pk,
+        profile_pic_url: item.profile_pic_url,
+      }
+    )
   }
+  return friends;
+}
+
+const getFollowings = async (): Promise<Friend[]> => {
+  const followingsFeed = ig.feed.accountFollowing(user.pk);
+  let friends: Friend[] = [];
+  const items = await followingsFeed.items();
+  console.log(`ITEM LENGTH IS: ${items.length}`)
+  for (const item of items) {
+    friends.push({
+      username: item.username,
+      full_name: item.full_name,
+      pk: item.pk,
+      profile_pic_url: item.profile_pic_url,
+    })
+  }
+  return friends
 }
 
 const removeAllCloseFriend = async () => {
-  const removeFriends = [1]
+  const removeFriends = ["4008089659", "3265313838"]
   await ig.friendship.setBesties({
     remove: removeFriends
   })
 }
 
+const saveCSV = (data: any, filepath: string) => {
+  const csvString = stringify(data, {
+    header: true
+  });
+  fs.writeFileSync(filepath, csvString);
+}
+
 const main = async () => {
   await login()
-  await getCloseFriends()
+  // await getCloseFriends()
+  // await removeAllCloseFriend()
+  const followings = await getFollowings();
+  saveCSV(followings, "csv/followings.csv")
 }
 
 main()
